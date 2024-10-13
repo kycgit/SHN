63be202738bc14c5ca7af182ef9a24b9cd273e83
kycgit <kyceye@gmail.com>
2024-10-13 17:10:55 +0900

    Update package.lisp

--- package.lisp
+++ package.lisp
@@ -2,5 +2,5 @@
 
 (defpackage #:shn
   (:use #:cl #:trivial-arguments)
-  (:export #:shn #:%e #:%c #:%G #:%N0 #:%mp #:%mn #:%me #:%ee #:%Mo #:%GMo #:%ly #:%au #:%pc #:π
+  (:export #:%e #:%c #:%G #:%N0 #:%mp #:%mn #:%me #:%ee #:%Mo #:%GMo #:%ly #:%au #:%pc #:π
 	   #:*shn-symbol* #:ln #:^ #:e^ #:toDeg #:inDeg #:√ #:1/ #:0- #:÷ #:×))
